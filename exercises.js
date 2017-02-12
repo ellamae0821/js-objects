99ad060acfed9ef780bdd048abe8e4a39e56052d
ella <ellamae207@gmail.com>
2017-02-11 16:22:09 -1000

    print processed order completed

--- exercises.js
+++ exercises.js
@@ -18,7 +18,14 @@ A literal object.
  "My dog `name` is `age` year old likes to `speak`."
  */ 
 
-
+var dog = {
+  name: "Chi-Chi",
+  age: 8,
+  vegeterian: false,
+  color: "brown",
+  speak: "woof",
+};
+console.log("My dog " + dog.name + " is " + dog.age + " year old likes to " + dog.speak+ ".");
 /*
 An empty object
    Declare a variable named `kicks` and assign it to an EMPTY object.
@@ -32,6 +39,17 @@ An empty object
   Console.log the object.
 */
 
+var kicks = {};
+
+kicks.brand ="Vans";
+kicks.color = "red";
+kicks.size = 8;
+kicks.buy = function(){
+  return "Hell Yeah!";
+};
+
+console.log(kicks);
+console.log(kicks.buy());
 
 /* 
    Declare a variable named `plainBox` and set its value to be an EMPTY object.
@@ -42,6 +60,19 @@ An empty object
 */
 
 
+var plainBox = {};
+
+plainBox.color = "white";
+plainBox.size = 20;
+plainBox.contents = [ ];
+
+
+
+
+
+
+
+
 /*
    Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
             - add a key named `model` and set its value to the name of an American car manufacturer.
@@ -50,6 +81,18 @@ An empty object
             - add a key named `driver` and set it to be `null`.
             - add a key named `passengers` and set it to be an empty array.
 */
+
+var stockCar = {
+  model: "GM",
+  year: 2013,
+  automaticTransmission: true,
+  driver: null,
+  passengers: [],
+
+
+
+}
+
 
 
 /*
@@ -67,6 +110,17 @@ An empty object
    the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just 
    the value at `name`, and just the value at `age`.
  */
+
+
+var plainPerson = {};
+function buildPerson (person, nameString, age){
+  person.name = nameString;
+  person.age=age;{
+  return person;
+    }
+}
+var completePerson=buildPerson(plainPerson, "Ella", 28);
+console.log(completePerson);
 
 
 /*
@@ -93,6 +147,85 @@ An empty object
             =====
             ...
  */
+/*
+var orders = 
+{ id date }
+
+several console.log 
+
+*/
+
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+/*
+Declare a new function named `printProcessedOrders`and set one parameter called `orders`. Within this function you 
+   will return a string that prints the order details to the page in the following format. */
+
+   function printProcessedOrders (orders){
+    for (var i=0; i<orders.length; i++)/*{
+      console.log("=====" + "\n" + "ID:" + orders[i].id + "\n" + "Purchase Date:" + orders[i].date+ "\n" + "Purchase Total:" + orders[i].total + "\n");
+   }*/
+                                          {
+      console.log("=====" );
+      console.log("ID:" + orders[i].id);
+      console.log("Purchase Date: " + orders[i].date);
+      console.log("Purchase Total: " + orders[i].total);                           
+                                          }
+}
+printProcessedOrders(arrayOfObjects);
+//console.log  ("====" + "\n" + arrayOfObjects.id + "\n" );
+/*console.log ("===" + "\n" + arrayOfObjects.id + "\n" + arrayOfObjects.date + "\n" + arrayOfObjects.total + "\n");*/
 
 
 /*
